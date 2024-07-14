1c1b91a7594e8cb8cc7f02ab5ec98f0594c6e9c1
Grisha Szep <grisha.szep@gmail.com>
2024-07-14 23:09:27 +0200

    onclick callback calls re_make on problem

--- src/BifurcationExplorer.jl
+++ src/BifurcationExplorer.jl
@@ -13,23 +13,26 @@ F(x, p) = @. p.μ + x - x^3 / 3
 opts = ContinuationPar(p_min=-1.0, p_max=1.0)
 problem = Observable(BifurcationProblem(F, [-2.0], (μ=-1.0,), (@lens _.μ)))
 
-branch = Observable{Vector{Point{2,Float32}}}([])
-bifurcations = Observable{Vector{Point{2,Float32}}}([])
+branch = Observable{Vector{Point{2,Float64}}}([])
+bifurcations = Observable{Vector{Point{2,Float64}}}([])
 
 # run continuation every time the problem changes
 on(problem) do problem
     iter = ContIterable(problem, PALC(), opts; verbosity=0)
     for state in iter
-
         point = (state.z.p, state.z.u[1])
+
         push!(branch[], point)
+        notify(branch)
 
         if detect_bifurcation(state)
+
             push!(bifurcations[], point)
             notify(bifurcations)
-        end
 
-        notify(branch)
+            @info "Bifurcation detected at $(point)"
+            break
+        end
         sleep(0.1)
     end
 end
@@ -45,14 +48,17 @@ app = App() do session::Session
         limits=(opts.p_min, opts.p_max, -3, 3),)
 
     lines_plot = lines!(ax, branch, color=:blue)
-    scatter_plot = scatter!(ax, bifurcations, color=:orange, markersize=10)
+    scatter_plot = scatter!(ax, bifurcations, color=:orange, markersize=12)
 
     # onclick bifurcation point event handler
     on(events(figure).mousebutton, priority=2) do event
         if event.button == Mouse.left && event.action == Mouse.press
-            plt, i = pick(figure)
-            if plt == scatter_plot
-                @info bifurcations[][i]
+            picked_plot, i = pick(figure)
+
+            if picked_plot == scatter_plot
+                p, u = bifurcations[][i]
+
+                problem[] = re_make(problem[]; u0=[u], params=(μ=p,))
                 return Consume(true)
             end
 
